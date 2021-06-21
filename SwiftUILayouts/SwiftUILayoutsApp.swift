8867b9dba77e4c03a6ce6ae4d059df9c99c37176
bogdanov <iceposhta@gmail.com>
2021-06-22 00:11:01 +0200

    Initial Commit

---
+++ SwiftUILayouts/SwiftUILayoutsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUILayoutsApp.swift
+//  SwiftUILayouts
+//
+//  Created by bogdanov on 22.06.21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUILayoutsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
